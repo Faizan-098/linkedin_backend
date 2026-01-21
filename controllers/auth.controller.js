67442908415792bdcbc5a83e82925f959f64fb89
Faizan-098 <faizan.imran607@gmail.com>
2026-01-22 00:45:07 +0500

    one change

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -60,8 +60,8 @@ export const signUp = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: "strict",
-      secure: process.env.NODE_ENVIRONMENT == "production",
+      sameSite: "none",
+      secure: process.env.NODE_ENVIRONMENT === "production",
     });
 
     // Return response
@@ -99,7 +99,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: "strict",
+      sameSite: "none",
       secure: process.env.NODE_ENVIRONMENT == "production",
     });
 
@@ -117,7 +117,7 @@ export const logout = async (req, res) => {
     res.clearCookie("token", {
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      sameSite: "strict",
+      sameSite: "none",
       secure: process.env.NODE_ENVIRONMENT == "production",
     });
     return res.status(200).json({ message: "Logout Successfully!" });
